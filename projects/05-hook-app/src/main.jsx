49414bbd7f0eadfa1a6629d809ad10931a56dae9
Efrain Garcia <efraingarcia_86@hotmail.com>
2024-03-05 19:45:09 -0700

    05-hook-app

--- projects/05-hook-app/src/main.jsx
+++ projects/05-hook-app/src/main.jsx
@@ -6,15 +6,19 @@ import './index.css'
 /* import CounterWuthCustomHook from './01-useState/CounterWuthCustomHook' */
 /* import SimpleForm from './02-useEffect/SimpleForm' */
 /* import FormWithCustomHook from './02-useEffect/FormWithCustomHook' */
-import MultipleCustomHook from './03-examples/MultipleCustomHook'
+/* import MultipleCustomHook from './03-examples/MultipleCustomHook' */
+/* import FocusScreen from './04-useRef/FocusScreen' */
+/* import {Layout} from './05-useLayoutEffect/Layout' */
+/* import Memorize from './06-memos/Memorize' */
+/* import MemoHook from './06-memos/MemoHook' */
+/* import CallbackHook from './06-memos/CallbackHook' */
+/* import { Padre } from './07-tarea-memo/Padre' */
+import TodoApp from './08-useReducer/TodoApp'
+
+/* import './08-useReducer/intro-reducer' */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <HooksApp /> */}
-    {/* <CounterApp /> */}
-    {/* <CounterWuthCustomHook /> */}
-    {/* <SimpleForm /> */}
-    {/* <FormWithCustomHook /> */}
-    <MultipleCustomHook />
+    <TodoApp />
   </React.StrictMode>,
 )
