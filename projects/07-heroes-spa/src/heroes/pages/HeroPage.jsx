1a3955cbbab7fee1e868113edd0165c29e5ee6e6
Efrain Garcia <efraingarcia_86@hotmail.com>
2024-03-18 22:06:35 -0700

    07-heroes-app hero detail

--- projects/07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ projects/07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,16 @@
-export const HeroesPage = () => {
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers/getHeroById";
+
+export const HeroPage = () => {
+    const { heroId } = useParams();
+
+    const hero = getHeroById(heroId);
+
+    if(!hero) {
+        return <Navigate to={"/marvel"} />
+    }
+
     return (
-        <div>HeroesPage</div>
+        <div>{hero.superhero}</div>
     )
 }
--- projects/07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
+++ projects/07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Navbar } from '../../ui'
-import { DcPage, HeroesPage, MarvelPage, SearchPage } from '../pages'
+import { DcPage, HeroPage, MarvelPage, SearchPage } from '../pages'
 
 export const HeroesRoutes = () => {
   return (
@@ -13,7 +13,7 @@ export const HeroesRoutes = () => {
                 <Route path="dc" element={<DcPage />} />
 
                 <Route path="search" element={<SearchPage />} />
-                <Route path="heroes" element={<HeroesPage />} />
+                <Route path="hero/:heroId" element={<HeroPage />} />
 
                 <Route path="/" element={<Navigate to={"/marvel"} />} />
             </Routes>
